387b22dda6549c01842596c386b182120aafa4e9
Damien Mathieu <42@dmathieu.com>
2026-09-22 11:46:25 +0200

    Setup tap with ocb formula (#5)
    Co-authored-by: Douglas Camata <159076+douglascamata@users.noreply.github.com>

---
+++ Formula/ocb.rb
@@ -0,0 +1,45 @@
+class Ocb < Formula
+  desc "OpenTelemetry Collector Builder - assemble custom OTel Collector distributions"
+  homepage "https://github.com/open-telemetry/opentelemetry-collector/tree/main/cmd/builder"
+  version "0.161.0"
+  license "Apache-2.0"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/cmd%2Fbuilder%2Fv#{version}/ocb_#{version}_darwin_arm64"
+      sha256 "0a52c4f87c30953836739559f5845d7672167f7f736affaa0afbd5b764bf131f"
+    end
+
+    on_intel do
+      url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/cmd%2Fbuilder%2Fv#{version}/ocb_#{version}_darwin_amd64"
+      sha256 "ede7d2f3a6b6812dffb09c5a4ee7e3ccbd660adceab3368a7fca8b3430625278"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/cmd%2Fbuilder%2Fv#{version}/ocb_#{version}_linux_arm64"
+      sha256 "87201f8aa3f931082a50f0ed3d69272f5741cfe1f794e2ef5877610fc1e7617c"
+    end
+
+    on_intel do
+      url "https://github.com/open-telemetry/opentelemetry-collector-releases/releases/download/cmd%2Fbuilder%2Fv#{version}/ocb_#{version}_linux_amd64"
+      sha256 "80c4bc4fa2c8050adb5901ddca2fd4b4f5c7c4db77c15a3c6cb27d85375c1bc6"
+    end
+  end
+
+  livecheck do
+    url :stable
+    regex(%r{^cmd/builder/v(\d+(?:\.\d+)+)$}i)
+    strategy :github_releases
+  end
+
+  def install
+    # The upstream binary is already named `ocb`, so we just install it directly.
+    bin.install stable.url.split("/").last => "ocb"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/ocb version 2>&1")
+  end
+end
